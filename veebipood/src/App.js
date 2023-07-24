d4f7a37396177b5e6df0148ec32c17f4cdb5bc1c
brokans <46932664+brokans@users.noreply.github.com>
2023-07-24 12:09:10 +0300

    React algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Avaleht from './pages/Avaleht';
+import Ostukorv from './pages/Ostukorv';
+import LisaToode from './pages/LisaToode';
+
+
+
+function App() {
+  return (
+    // className on reactile omane, kogu kood peab olema div'i vahel
+    <div className="App">
+      <Link to="avaleht">
+        <img className="pilt" src="https://th.bing.com/th/id/R.5ecd5ae33c75e812c2e0b3b95d430d41?rik=GurWTZ4t7U5C5A&pid=ImgRaw&r=0" alt="" />
+      </Link>
+
+      <Link to="lisa-toode">
+        <button className="nupp">Lisa toode</button>
+      </Link>
+
+      <Link to="ostukorv">
+        <button className="nupp">Ostukorv</button>
+      </Link>
+      
+      <Routes>
+        <Route path="avaleht" element={<Avaleht/>} />;
+        <Route path="ostukorv" element={<Ostukorv/>} />;
+        <Route path="lisa-toode" element={<LisaToode/>} />;
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ veebipood/src/pages/Ostukorv.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// rfce lühend teeb põhja
+function Ostukorv() {
+  return (
+    <div>Ostukorv</div>
+  )
+}
+
+export default Ostukorv
