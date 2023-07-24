a65bdba5ad992ac3aa1bbaf332f557806598fed0
brokans <46932664+brokans@users.noreply.github.com>
2023-07-24 14:00:26 +0300

    Kodutöö 1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupp">Click</button>
+      <p className="tekst-s"><em>SinineTekst</em></p>
+      <p className="tekst-r"><em><strong>RohelineTekst</strong></em></p>
+      <div className="pildi-konteiner">
+        <img className="pilt" src="https://th.bing.com/th?id=OSK.HERODT5XuzICKIfGtOvKhvOn_89SFz9cwTXwdNvQuewlue0&w=296&h=176&c=1&rs=2&o=6&dpr=1.5&pid=SANGAM" alt="" />   
+      </div>
+    </div>
+  );
+}
+
+export default App;
