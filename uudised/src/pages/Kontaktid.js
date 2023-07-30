ff40f7dac22ec56a6f6b87aa43dd51da0b752079
brokans <46932664+brokans@users.noreply.github.com>
2023-07-30 15:24:53 +0300

    Uudiste lehe muudatused
    Meist lehel numbrid nuppudega ja võta ühendust lehel algselt numbrid peidetud, klikkimisega on/off.

--- uudised/src/pages/Kontaktid.js
+++ uudised/src/pages/Kontaktid.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
+
 
 function Kontaktid() {
+const[n2itaTelKristiine, mN2itaTelKristiine] = useState(false);
+const[n2itaTelViru, mN2itaTelViru] = useState(false);
+const[n2itaTelLouna, mN2itaTelLouna] = useState(false);
+
+
   return ( <div>
     <div>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</div>
     <div>Võta meiega ühendust:</div>
     <br />
-    <div>Kristiine Keskus</div>
-    <div>+372732776</div>
-    <div>Endla 45</div>
+    <div onClick={() => mN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine Keskus</div>
+    {n2itaTelKristiine && <div>+372732776</div>}
+    <div>Endla 45, Tallinn</div>
     <br />
-    <div>Viru Keskus</div>
-    <div>+372137724</div>
-    <div>Viru Väljak 4</div>
+    <div onClick={() => mN2itaTelViru(!n2itaTelViru)}>Viru Keskus</div>
+    {n2itaTelViru && <div>+372137724</div>}
+    <div>Viru Väljak 4, Tallinn</div>
     <br />
-    <div>Lõuna Keskus</div>
-    <div>+372878678</div>
-    <div>Jaama 207</div>
+    <div onClick={() => mN2itaTelLouna(!n2itaTelLouna)}>Lõuna Keskus</div>
+    {n2itaTelLouna && <div>+372878678</div>}
+    <div>Jaama 207, Tartu</div>
   </div>);
 }
 
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 function Meist() {
+  const[kontakt, n2itaKontakti] = useState("");
+  const[kontakt1, n2itaKontakti1] = useState("");
+  const[kontakt2, n2itaKontakti2] = useState("");
+  const[kontakt3, n2itaKontakti3] = useState("");
+
+
   return (<div>
         <div>See on meist leht, nähtav localhost:3000/meist aadressil</div>
         <div>Meie töötajad:</div>
         <br />
-        <div>Timo Timon</div>
-        <div>+3727689074</div>
+        <div>Hash Tag</div>
+        {/* <div>+3727689074</div> */}
         <div>Uudiste ankur</div>
+        <button onClick={() => n2itaKontakti('+3727689074')}>Võta ühendust</button>
+        <div>Tema kontakt: {kontakt}</div>
         <br />
         <div>Kaja Kajakas</div>
-        <div>+3726782390</div>
+        {/* <div>+3726782390</div> */}
         <div>Meedia organisaator</div>
+        <button onClick={() => n2itaKontakti1('+3726782390')}>Võta ühendust</button>
+        <div>Tema kontakt: {kontakt1}</div>
         <br />
         <div>Ott Karu</div>
-        <div>+37287366023</div>
+        {/* <div>+37287366023</div> */}
         <div>Helidisainer</div>
+        <button onClick={() => n2itaKontakti2('+37287366023')}>Võta ühendust</button>
+        <div>Tema kontakt: {kontakt2}</div>
         <br />
         <div>Jaana Lind</div>
-        <div>+3723998467</div>
+        {/* <div>+3723998467</div> */}
         <div>Infotehnoloog</div>
+        <button onClick={() => n2itaKontakti3('+3723998467')}>Võta ühendust</button>
+        <div>Tema kontakt: {kontakt3}</div>
     </div>);
 }
 
