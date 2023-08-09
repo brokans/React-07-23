e1405f88559fb98efdd7230f186862866c1daa9b
brokans <46932664+brokans@users.noreply.github.com>
2023-08-09 13:35:17 +0300

    Arrayde manipuleerimine

--- tagasiside/src/App.js
+++ tagasiside/src/App.js
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Tagasisided from './pages/Tagasisided';
 import TagasisideAndja from './pages/TagasisideAndja';
+import Sorteerimine from './pages/Sorteerimine';
 
 
 
@@ -18,11 +19,15 @@ function App() {
       <Link to="/andjad">
         <button>Vaata kes tagasisidet on andnud</button>
       </Link>
+      <Link to="/sorteerimine">
+        <button>Sorteeri</button>
+      </Link>
 
       <Routes>
         <Route path="/" exact element = {<div>Tere</div>}/>
         <Route path="/tagasiside" exact element = {< Tagasisided />}/>
         <Route path="/andjad" exact element = {< TagasisideAndja />}/>
+        <Route path="/sorteerimine" exact element = {< Sorteerimine />}/>
       </Routes>
     </div>
   );
