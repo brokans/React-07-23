570603fca7b62074f4869d78be313786fa7f1cda
brokans <46932664+brokans@users.noreply.github.com>
2023-08-14 16:01:58 +0300

    Veateadete eemaldamine

--- joogipood/src/pages/Halda.js
+++ joogipood/src/pages/Halda.js
@@ -3,19 +3,13 @@ import {useRef, useState } from 'react';
 import joogidFail from "../data/joogidFail.json"
 
 function Halda() {
-    
-    
       const [joogid, uJoogid] = useState(joogidFail);
-      const joogidRef = useRef();
     
       function kustuta(index) {
         joogid.splice(index, 1);
         uJoogid(joogid.slice());
       }
     
-      
-
-
   return (
     <div>
         Tooted:
